738ea087e8c11ae345967e9cd4a3bb4545553993
Tom Molesworth <TEAM@cpan.org>
2017-05-11 22:42:55 +0800

    Make repo example more useful

--- examples/repos.pl
+++ examples/repos.pl
@@ -1,7 +1,6 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use feature qw(say);
 
 use IO::Async::Loop;
 use Net::Async::Github;
@@ -21,23 +20,16 @@ $gh->core_rate_limit->remaining->subscribe(sub {
     printf "Have %d Github requests remaining\n", $_;
 });
 
-open my $missing, '>:encoding(UTF-8)', 'missing.lst' or die $!;
-# List of all repos
-my $user = shift;
-my $repos = $gh->repos($user ? (owner => $user) : ())
-    ->filter(sub { $_->{owner}{login} eq 'regentmarkets' })
-    ->each(sub {
-        printf "* %s has %d open issues and %d forks\n",
-            $_->name,
-            $_->open_issues_count,
-            $_->forks_count;
-            use Data::Dumper;
-            print Dumper($_->owner);
-        printf ">>> %s/%s\n", $_->owner->{login}, $_->name;
-        $missing->print($_->name . "\n") unless -d '/home/git/regentmarkets/' . $_->name;
-    })
+# Show lots of information about repos
+my $user = $gh->current_user->get;
+my $repos = $gh->repos(
+    owner => $user->login
+)
 # ... and aggregate stats
     ->apply(sub {
+        $_->sprintf_methods("* %s has %d open issues and %d forks", qw(name open_issues_count forks_count))
+            ->say
+    }, sub {
         $_->count
             ->each(sub {
                 printf "Total of %d repos found\n", $_;
@@ -58,9 +50,8 @@ my $repos = $gh->repos($user ? (owner => $user) : ())
 # also get branch info
     ->apply(sub {
         $_->flat_map('branches')
-            ->each(sub {
-                printf "found branch called %s\n", $_->name
-            })
+            ->sprintf_methods("=> has a branch called %s", qw(name))
+            ->say;
     })
     ->await;
 
